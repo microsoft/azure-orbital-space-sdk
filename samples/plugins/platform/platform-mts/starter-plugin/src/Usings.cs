82379bfb5340849b9b64440ee1c82d00ed44175c
Ryan Campbell <bigtallcampbell@gmail.com>
2024-06-24 22:10:23 +0000

    platform-mts initial setup

---
+++ samples/plugins/platform/platform-mts/starter-plugin/src/Usings.cs
@@ -0,0 +1,5 @@
+global using Microsoft.Azure.SpaceFx;
+global using Microsoft.Azure.SpaceFx.MessageFormats.Common;
+global using Microsoft.Azure.SpaceFx.MessageFormats.HostServices.Sensor;
+global using Microsoft.Extensions.Logging;
+global using System.Collections.Concurrent;
