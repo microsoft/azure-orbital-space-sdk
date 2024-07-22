4609739d0a6a2f2daa301ba69fa4e10e7c50b777
Brad Armstrong <barmstrong@microsoft.com>
2024-07-22 19:19:33 +0000

    removes hostsvc-sensor/cpp-sample-plugin

--- samples/plugins/hostsvc/hostsvc-sensor/cpp-sample-plugin/src/Usings.cs
+++
@@ -1,6 +0,0 @@
-global using Microsoft.Azure.SpaceFx;
-global using Microsoft.Azure.SpaceFx.MessageFormats.Common;
-global using Microsoft.Azure.SpaceFx.MessageFormats.HostServices.Sensor;
-global using Microsoft.Extensions.Logging;
-global using System.Collections.Concurrent;
-global using System.Runtime.InteropServices;
